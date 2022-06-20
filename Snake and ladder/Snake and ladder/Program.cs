a9737325d96b98ded643aeaeae2786bb6a9ebc87
Pratikbhandurge2022 <pratikbhandurge2222@gmail.com>
2022-06-20 16:02:23 +0530

    uc1

--- Snake and ladder/Snake and ladder/Program.cs
+++ Snake and ladder/Snake and ladder/Program.cs
@@ -6,7 +6,10 @@ namespace Snake_and_ladder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Snake and Ladder game played with single player at start position 0");
+            int position = 0;
+            Console.WriteLine("Position is " + position);
         }
     }
+
 }
